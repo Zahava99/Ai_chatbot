70a7ed6c2edc17c7997491152fcba34ebd99ac38
AnhNguyen21640 <nvhanhh6@gmail.com>
2026-06-29 14:34:12 +0700

    Update api.js

--- src/config/api.js
+++ src/config/api.js
@@ -3,7 +3,7 @@
 import { getAccessToken, ensureFreshToken } from "@/features/auth/api/authUtils";
 
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  BASE_URL: import.meta.env.VITE_API_URL || "http://localhost:5024",
   TIMEOUT: 10000,
 };
 
